5724d31b45cf0f8b2e70fedf5cb544caa9729681
lzm <liuzhaoming@guoquan.cn>
2025-03-28 10:44:51 +0800

    footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -59,7 +59,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © 2020 Powered by <a className='hover:underline' href="" target="_blank">程序员毛毛哥</a>
+                        © 2025 Powered by <a className='hover:underline' href="" target="_blank">程序员毛毛哥</a>
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
